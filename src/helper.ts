93e3b84f3a36aaec0e71e52733346fd0f99a24ae
Ni Tianzhen <nitianzhen1210@hotmail.com>
2025-03-17 18:02:40 +0800

    feat: add local amplitude gaussian

---
+++ src/helper.ts
@@ -0,0 +1,43 @@
+import { BufferAttribute, BufferGeometry } from 'three';
+
+export function createGridGeometry(
+  width: number,
+  height: number,
+  widthSegments: number,
+  heightSegments: number
+) {
+  const geometry = new BufferGeometry();
+  const [cornerX, cornerY] = [-width / 2, -height / 2];
+  const [stepX, stepY] = [width / widthSegments, height / heightSegments];
+
+  const vertices = [];
+  for (let i = 0; i <= widthSegments; i += 1) {
+    const x = cornerX + i * stepX;
+    for (let j = 0; j <= heightSegments; j += 1) {
+      const y = cornerY + j * stepY;
+      vertices.push(x, y, 0);
+    }
+  }
+  const indices = [];
+  for (let i = 0; i < widthSegments; i += 1) {
+    for (let j = 0; j < heightSegments; j += 1) {
+      const a = i * (heightSegments + 1) + j;
+      const b = a + heightSegments + 1;
+      const c = a + 1;
+      const d = b + 1;
+      indices.push(a, b, b, d, d, c, c, a);
+    }
+  }
+
+  geometry.setAttribute(
+    'position',
+    new BufferAttribute(new Float32Array(vertices), 3)
+  );
+  geometry.setIndex(indices);
+
+  return geometry;
+}
+
+export function gaussian(x: number, a = 1, mu = 0, sigma = 1) {
+  return a * Math.exp(-((x - mu) ** 2) / (2 * sigma ** 2));
+}
--- src/main.ts
+++ src/main.ts
@@ -7,23 +7,25 @@ import {
   PointsMaterial,
   LineBasicMaterial,
   LineSegments,
-  BufferGeometry,
   BufferAttribute,
 } from 'three';
 import Stats from 'three/addons/libs/stats.module.js';
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import { createGridGeometry, gaussian } from './helper';
 import './style.css';
 
 const gui = new GUI();
 const configuration = {
   speed: 1,
   scanSpeed: 1,
-  amplitude: 1,
+  amplitude: 0.5,
+  localAmplitude: 0,
 };
 const folder = gui.addFolder('Multiplier');
 folder.add(configuration, 'speed', 0, 2).name('Scroll Speed');
 folder.add(configuration, 'scanSpeed', 0, 2).name('Scanline Speed');
 folder.add(configuration, 'amplitude', 0, 2).name('Amplitude');
+folder.add(configuration, 'localAmplitude', 0, 3).name('Local Amplitude');
 
 const scene = new Scene();
 scene.background = new Color('black');
@@ -34,44 +36,6 @@ const camera = new PerspectiveCamera(
 );
 camera.position.set(0, 2, 4);
 camera.lookAt(scene.position);
-
-function createGridGeometry(
-  width: number,
-  height: number,
-  widthSegments: number,
-  heightSegments: number
-) {
-  const geometry = new BufferGeometry();
-  const [cornerX, cornerY] = [-width / 2, -height / 2];
-  const [stepX, stepY] = [width / widthSegments, height / heightSegments];
-
-  const vertices = [];
-  for (let i = 0; i <= widthSegments; i += 1) {
-    const x = cornerX + i * stepX;
-    for (let j = 0; j <= heightSegments; j += 1) {
-      const y = cornerY + j * stepY;
-      vertices.push(x, y, 0);
-    }
-  }
-  const indices = [];
-  for (let i = 0; i < widthSegments; i += 1) {
-    for (let j = 0; j < heightSegments; j += 1) {
-      const a = i * (heightSegments + 1) + j;
-      const b = a + heightSegments + 1;
-      const c = a + 1;
-      const d = b + 1;
-      indices.push(a, b, b, d, d, c, c, a);
-    }
-  }
-
-  geometry.setAttribute(
-    'position',
-    new BufferAttribute(new Float32Array(vertices), 3)
-  );
-  geometry.setIndex(indices);
-
-  return geometry;
-}
 
 const geometry = createGridGeometry(6, 4, 120, 80);
 const pos = geometry.getAttribute('position');
@@ -98,17 +62,24 @@ renderer.setAnimationLoop((t: number) => {
   for (let i = 0; i < pos.count; i += 1) {
     const x = pos.getX(i);
     const y = pos.getY(i);
+
+    const distanceToCenter = Math.sqrt(x ** 2 + y ** 2);
+    const gaussianValue =
+      gaussian(distanceToCenter, 1, 0, 0.6) * configuration.localAmplitude;
+
     const z =
       ((Math.sin((x + (t * configuration.speed) / 2000) * 3) *
         Math.sin(y * 3)) /
         3) *
-      configuration.amplitude;
+      configuration.amplitude *
+      (1 + gaussianValue);
 
     pos.setZ(i, z);
+    // pos.setZ(i, gaussianValue);
 
     const scanlineX = (((t * configuration.scanSpeed) / 1000) % 8) - 4; // -4 to 4
-    const distance = Math.abs(x - scanlineX);
-    const opacity = Math.exp(-distance * 12) * 255;
+    const distanceToScanline = Math.abs(x - scanlineX);
+    const opacity = Math.exp(-distanceToScanline * 12) * 255;
     colors[i * 3] = opacity;
     colors[i * 3 + 1] = opacity;
     colors[i * 3 + 2] = opacity;
